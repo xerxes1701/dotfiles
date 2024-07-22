da6137efd276a967944aa184d9e81fe25cf42ab4
Michael Gawlik <michaelgawlik1983@gmail.com>
2024-07-22 20:12:33 +0200

    changed color scheme to catppuccin

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,13 @@
 -- the colorscheme should be available when starting Neovim
--- https://github.com/goolord/alpha-nvim
+-- https://github.com/catppuccin/nvim
 
-return {
-    "sainnhe/everforest",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme everforest]])
-    end,
-  }
+return { 
+  "catppuccin/nvim", 
+  name = "catppuccin", 
+  priority = 1000,
+  config = function()
+    require('catppuccin').setup({
+    })
+    vim.cmd.colorscheme('catppuccin')
+  end,
+}

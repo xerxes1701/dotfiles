503741011cb55940a69df92c52bca8f24dbda3a7
Michael Gawlik <michaelgawlik1983@gmail.com>
2024-07-22 19:23:29 +0200

    moved neo git config into plugin file

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -2,15 +2,19 @@
 -- https://github.com/NeogitOrg/neogit
 
 return {
-	  'NeogitOrg/neogit',
-	  tag = 'v0.0.1', -- neovim 0.9.x compatible
-	  dependencies = {
-		  'nvim-lua/plenary.nvim',
-		  'sindrets/diffview.nvim',
-		  'nvim-telescope/telescope.nvim',
-	  },
-	  config = function()
-		  require("config.neogit").setup()
-    end,
-  }
+  'NeogitOrg/neogit',
+	tag = 'v0.0.1', -- neovim 0.9.x compatible
+	dependencies = {
+	  'nvim-lua/plenary.nvim',
+		'sindrets/diffview.nvim',
+		'nvim-telescope/telescope.nvim',
+	},
+	config = function()
+	  require("neogit").setup({
+      integrations = { 
+        diffview = true 
+      }
+    })
+  end,
+}
 

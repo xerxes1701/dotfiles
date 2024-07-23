785f074c07ac01f38629c9601c054ae930c37ae7
Michael Gawlik <michaelgawlik1983@gmail.com>
2024-07-23 23:46:12 +0200

    added lualine

---
+++ .config/nvim/lua/plugins/status-line.lua
@@ -0,0 +1,9 @@
+-- status line
+-- https://github.com/nvim-lualine/lualine.nvim
+
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = 'nvim-web-devicons',
+  event = 'VimEnter',
+  opts = {},
+}

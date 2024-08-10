0b26516096bd5b99842844f963d8ec5873181ac0
Michael Gawlik <michaelgawlik1983@gmail.com>
2024-08-11 00:54:08 +0200

    added yazi, nvim-flash

---
+++ .config/nvim/lua/plugins/easy-motion.lua
@@ -0,0 +1,17 @@
+-- easy motion / jump any where
+-- https://github.com/folke/flash.nvim
+
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  ---@type Flash.Config
+  opts = {},
+  -- stylua: ignore
+  keys = {
+    { "<leader>jj", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "<leader>jt", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "<leader>jr", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "<leader>js", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+} 

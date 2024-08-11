9092111bf42bb8b6b57e045afa03318f9da4fe32
Michael Gawlik <michaelgawlik1983@gmail.com>
2024-08-11 23:35:44 +0200

    configured telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,15 +2,40 @@
 -- https://github.com/nvim-telescope/telescope.nvim
 
 return { 
-    'nvim-telescope/telescope.nvim', 
-    tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    keys =
-    {
-      {'<leader>fg', "<cmd>Telescope live_grep<cr>", desc = "Live grep"},
-      {'<leader>ff', "<cmd>Telescope find_files<cr>", desc = "Find file"},
-      {'<leader>fd', "<cmd>Telescope find_files hidden=true<cr>", desc = "Find hidden file"},
-      {'<leader>fb', "<cmd>Telescope buffers<cr>", desc = "Find buffer"},
-      {'<leader>fh', "<cmd>Telescope help_tags<cr>", desc = "Find tag"},
-    },
-  }
+  'nvim-telescope/telescope.nvim', 
+  tag = '0.1.8',
+  dependencies = {
+    'nvim-lua/plenary.nvim', 
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    'nvim-tree/nvim-web-devicons',
+  },
+  config = function()
+    local telescope = require('telescope')
+    local actions = require('telescope.actions')
+
+    telescope.setup({
+      defaults = {
+        path_display = { 'smart' },
+        mappings = {
+          i = {
+            ['<C-k>'] = actions.move_selection_previous,
+            ['<C-j>'] = actions.move_selection_next,
+            ['<C-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
+          }
+        }
+      },
+    })
+    
+    -- load native fzf extension
+    telescope.load_extension('fzf')
+  end,
+  keys =
+  {
+    {'<leader>fg', "<cmd>Telescope live_grep<cr>", desc = "fuzzy find live grep"},
+    {'<leader>fr', "<cmd>Telescope oldfiles<cr>", desc = "fuzzy find recent"},
+    {'<leader>ff', "<cmd>Telescope find_files<cr>", desc = "fuzzy find files"},
+    {'<leader>fd', "<cmd>Telescope find_files hidden=true<cr>", desc = "fuzzy find hidden files"},
+    {'<leader>fb', "<cmd>Telescope buffers<cr>", desc = "fuzzy find buffers"},
+    {'<leader>fh', "<cmd>Telescope help_tags<cr>", desc = "fuzzy find help tags"},
+  },
+}

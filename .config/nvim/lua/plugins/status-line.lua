0499a7eaa5ad267c9b8ecca93b1c7c66caa5521c
Michael Gawlik <michaelgawlik1983@gmail.com>
2024-08-14 00:18:22 +0200

    moved Lazy update notificatio into status bar

--- .config/nvim/lua/plugins/status-line.lua
+++ .config/nvim/lua/plugins/status-line.lua
@@ -5,5 +5,22 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = 'nvim-web-devicons',
   event = 'VimEnter',
-  opts = {},
+  config = function()
+    local lazy_status = require('lazy.status')
+
+    require('lualine').setup({
+      sections = {
+        lualine_x = {
+          {
+            lazy_status.updates,
+            lazy_status.has_updates,
+            color = { fg = '#ff9e64' },
+          },
+          { 'encoding' },
+          { 'fileformat' },
+          { 'filetype' },
+        }
+      }
+    })
+  end,
 }

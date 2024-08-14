2e3e6168a40b710f7e5ace935713ccbe0034f1dd
Michael Gawlik <michaelgawlik1983@gmail.com>
2024-08-15 01:36:35 +0200

    enabled incremental selection, autotaggin

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -14,6 +14,18 @@ return {
       indent = {
         enable = true,
       },
+      autotag = {
+        enable = true,
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<C-space>',
+          node_incremental = '<C-space>',
+          scope_incremental = false,
+          node_decremental = '<bs>',
+        },
+      },
     })
   end,
 }

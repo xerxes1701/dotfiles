507b39bdd6374a4f74d6dfe120d01f94c686a919
Michael Gawlik <michaelgawlik1983@gmail.com>
2024-08-15 21:50:21 +0200

    added nvim-autotag

---
+++ .config/nvim/lua/plugins/autotag.lua
@@ -0,0 +1,17 @@
+-- auto-closing HTML like tags
+-- https://github.com/windwp/nvim-ts-autotag
+
+return {
+  'windwp/nvim-ts-autotag',
+  opts = {
+    opts = {
+      enable_close = true, -- Auto close tags
+      enable_rename = true, -- Auto rename pairs of tags
+      enable_close_on_slash = true -- Auto close on trailing </
+    },
+    per_filetype = {
+      ["html"] = {
+      }
+    },
+  },
+}

4c06a6cac62c0a0e1f20e74fbe3dc1bbe70a2389
Michael Gawlik <michaelgawlik1983@gmail.com>
2024-08-15 11:36:12 +0200

    changed flash highlighing, keybinds

--- .config/nvim/lua/plugins/easy-motion.lua
+++ .config/nvim/lua/plugins/easy-motion.lua
@@ -6,11 +6,26 @@ return {
   tag = 'v2.1.0',
   event = "VeryLazy",
   ---@type Flash.Config
-  opts = {},
+  opts = {
+    modes = {
+      treesitter = {
+        label = {
+          rainbow = {
+            enabled = true,
+            shade = 5,
+          },
+        },
+        highlight = {
+          -- disable syntax highlighting while searching
+          backdrop = true,
+        },
+      },
+    },
+  },
   -- stylua: ignore
   keys = {
-    { "<leader>jj", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "<leader>jt", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "<leader>jr", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "<leader>js", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },

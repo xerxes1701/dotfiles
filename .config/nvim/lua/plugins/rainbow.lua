3a70f99702e5c1952009a8a23ce02b46129784fb
Michael Gawlik <michaelgawlik1983@gmail.com>
2024-08-15 22:59:29 +0200

    added nvim rainbow delimiters

---
+++ .config/nvim/lua/plugins/rainbow.lua
@@ -0,0 +1,10 @@
+-- rainbow brackets
+-- https://github.com/HiPhish/rainbow-delimiters.nvim
+
+return {
+  'hiphish/rainbow-delimiters.nvim',
+  config = function()
+    require('rainbow-delimiters.setup').setup({
+    })
+  end,
+}

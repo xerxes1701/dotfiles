59636d7423cfc5e95eda57ea9b08e02329481d9f
Michael Gawlik <michaelgawlik1983@gmail.com>
2024-08-20 23:54:24 +0200

    added nvim autopairs

---
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -0,0 +1,27 @@
+-- autocompete () {} [] `` '' ""
+-- https://github.com/windwp/nvim-autopairs 
+
+return {
+  'windwp/nvim-autopairs',
+  event = { 'InsertEnter' },
+  dependencies = { 
+    'hrsh7th/nvim-cmp',
+  },
+  config = function()
+    local autopairs = require('nvim-autopairs')
+    local cmp = require('cmp')
+    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+    
+    autopairs.setup({
+      check_ts = true, -- enable treesitter,
+      ts_config = {
+        lua = { 'string' }, -- don't add pairs in lua string
+        javascript = { 'template_string' }, -- don't add pairs in javascript template_strings
+      },
+    })
+
+    -- make autopairs and completion work together
+    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+  end,
+}
+

8c8f05ef534690778610289400a02219df78375a
Michael Gawlik <michaelgawlik1983@gmail.com>
2024-08-24 17:26:53 +0200

    readded nvim surround

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -1,19 +1,23 @@
--- add/change/delete surrounding pairs 
--- https://github.com/kylechui/nvim-surround
--- 
--- Old text                    Command         New text
--- ----------------------------------------------------------------------------
--- surr*ound_words             ysiw)           (surround_words)
--- *make strings               ys$"            "make strings"
--- [delete ar*ound me!]        ds]             delete around me!
--- remove <b>HTML t*ags</b>    dst             remove HTML tags
--- 'change quot*es'            cs'"            "change quotes"
--- <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
--- delete(functi*on calls)     dsf             function calls
+--- add/change/delete surrounding pairs 
+--- https://github.com/kylechui/nvim-surround
+--- 
+--- Old text                    Command         New text
+--- ----------------------------------------------------------------------------
+--- surr*ound_words             ysiw)           (surround_words)
+--- *make strings               ys$"            "make strings"
+--- [delete ar*ound me!]        ds]             delete around me!
+--- remove <b>HTML t*ags</b>    dst             remove HTML tags
+--- 'change quot*es'            cs'"            "change quotes"
+--- <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
+--- delete(functi*on calls)     dsf             function calls
 
 return {
-  'kyleshui/nvim-surround',
-  event = { 'BufReadPre', 'BufNewFile' },
-  version = '*',
-  config = true,
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
 }

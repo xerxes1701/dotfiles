971060e42606b665f6193097198ee7bed3f9a183
Michael Gawlik <michaelgawlik1983@gmail.com>
2024-08-25 01:17:53 +0200

    added nvim markdown-preview

---
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,13 @@
+-- render content of a markdown file in a synchronized browser window
+-- https://github.com/iamcco/markdown-preview.nvim
+
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && yarn install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  ft = { "markdown" },
+}
+

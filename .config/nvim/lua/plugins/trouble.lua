3ecf180d09c4b87459d09527070eadd2932f2009
Michael Gawlik <michaelgawlik1983@gmail.com>
2024-08-25 21:30:35 +0200

    added nvim trouble

---
+++ .config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,44 @@
+-- A pretty list for showing diagnostics, references, telescope results, quickfix and location lists to help you solve all the trouble your code is causing.
+-- https://github.com/folke/trouble.nvim
+
+return {
+  'folke/trouble.nvim',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+    'folke/todo-comments.nvim',
+  },
+  cmd = "Trouble",
+  opts = {},
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+  },
+}

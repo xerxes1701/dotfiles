6dabcafde7b0e70b08a2dbe1ff3ab905942e31f9
Michael Gawlik <michaelgawlik1983@gmail.com>
2024-08-26 23:34:37 +0200

    added nvim color scheme integrations

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,28 +1,31 @@
 -- the colorscheme should be available when starting Neovim
 -- https://github.com/catppuccin/nvim
 
-return { 
-  "catppuccin/nvim", 
-  name = "catppuccin", 
-  tag = 'v1.9.0',
-  priority = 1000,
-  config = function()
-    require('catppuccin').setup({
-      transparent_background = true,
-      term_colors = true,
-      integrations = {
-        bufferline = true,
-        cmp = true,
-        gitsigns = true,
-        mason = true,
-        notify = true,
-        nvimtree = true,
-        symbols_outline = true,
-        telescope = true,
-        treesitter = true,
-        treesitter_context = true,
-      },
-    })
-    vim.cmd.colorscheme('catppuccin')
-  end,
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	tag = "v1.9.0",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			transparent_background = true,
+			term_colors = true,
+			integrations = {
+				bufferline = true,
+				cmp = true,
+				gitsigns = true,
+				mason = true,
+				notify = true,
+				nvimtree = true,
+				symbols_outline = true,
+				telescope = {
+					enabled = true,
+				},
+				treesitter = true,
+				treesitter_context = true,
+				which_key = true,
+			},
+		})
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }

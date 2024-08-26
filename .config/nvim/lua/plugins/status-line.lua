b74a82c9115e01a98429029d5123700d834d57b3
Michael Gawlik <michaelgawlik1983@gmail.com>
2024-08-26 23:32:34 +0200

    fixed nvim status line plugin indicator

--- .config/nvim/lua/plugins/status-line.lua
+++ .config/nvim/lua/plugins/status-line.lua
@@ -2,25 +2,30 @@
 -- https://github.com/nvim-lualine/lualine.nvim
 
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = 'nvim-web-devicons',
-  event = 'VimEnter',
-  config = function()
-    local lazy_status = require('lazy.status')
+	"nvim-lualine/lualine.nvim",
+	dependencies = "nvim-web-devicons",
+	event = "VimEnter",
+	config = function()
+		local lazy_status = require("lazy.status")
 
-    require('lualine').setup({
-      sections = {
-        lualine_x = {
-          {
-            lazy_status.updates,
-            lazy_status.has_updates,
-            color = { fg = '#ff9e64' },
-          },
-          { 'encoding' },
-          { 'fileformat' },
-          { 'filetype' },
-        }
-      }
-    })
-  end,
+		require("lualine").setup({
+			options = {
+				theme = "catppuccin",
+			},
+			sections = {
+				lualine_x = {
+					{
+						function()
+							return lazy_status.updates() or ""
+						end,
+						lazy_status.has_updates,
+						color = { fg = "#ff9e64" },
+					},
+					{ "encoding" },
+					{ "fileformat" },
+					{ "filetype" },
+				},
+			},
+		})
+	end,
 }

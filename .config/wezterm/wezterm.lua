3d81e0cbf1947457d14fa7767529f3a628e7b9f7
Michael Gawlik <michaelgawlik1983@gmail.com>
2024-08-26 23:30:55 +0200

    added wezterm CTRL-V shortcut

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,26 +1,25 @@
-local w = require 'wezterm'
+local w = require("wezterm")
 local cfg = w.config_builder()
+local act = w.action
 
 function os_name()
-    return package.config:sub(1,1) == "\\" and "win" or "unix"
+	return package.config:sub(1, 1) == "\\" and "win" or "unix"
 end
 
+if os_name() == "win" then
+	cfg.default_prog = { "ubuntu" }
 
-if os_name() == 'win' then
-	cfg.default_prog = { 'ubuntu' }
+	function is_wm_running()
+		local command = "C:/code/CheckWM/bin/Debug/net8.0/CheckWM.exe"
+		-- find a way to execute this, without a having a terminal window poping up
+		-- return os.execute(command)
+		return false
+	end
 
-  function is_wm_running()
-    local command = 'C:/code/CheckWM/bin/Debug/net8.0/CheckWM.exe'
-    -- find a way to execute this, without a having a terminal window poping up
-    -- return os.execute(command)
-    return false
-  end
-
-  if is_wm_running() then
-    cfg.window_decorations = "RESIZE"
-  end
+	if is_wm_running() then
+		cfg.window_decorations = "RESIZE"
+	end
 end
-
 
 cfg.enable_tab_bar = false
 -- cfg.window_background_opacity = 0
@@ -30,43 +29,48 @@ cfg.enable_tab_bar = false
 cfg.color_scheme = "Catppuccin Mocha" -- or Macchiato, Frappe, Latte
 
 cfg.keys = {
-  -- Bind 'Ctrl + B' to toggle window decoration to "RESIZE"
-  {
-    key = 'b',
-    mods = 'CTRL',
-    action = w.action_callback(function(win, pane)
-      local overrides = win:get_config_overrides() or {}
-      if overrides.window_decorations == "RESIZE" then
-        overrides.window_decorations = "TITLE | RESIZE"
-      else
-        overrides.window_decorations = "RESIZE"
-      end
-      win:set_config_overrides(overrides)
-    end),
-  },
+	-- Bind 'Ctrl + B' to toggle window decoration to "RESIZE"
+	{
+		key = "b",
+		mods = "CTRL",
+		action = w.action_callback(function(win, pane)
+			local overrides = win:get_config_overrides() or {}
+			if overrides.window_decorations == "RESIZE" then
+				overrides.window_decorations = "TITLE | RESIZE"
+			else
+				overrides.window_decorations = "RESIZE"
+			end
+			win:set_config_overrides(overrides)
+		end),
+	},
+	-- paste from the clipboard
+	{ key = "V", mods = "CTRL", action = act.PasteFrom("Clipboard") },
+
+	-- paste from the primary selection
+	{ key = "V", mods = "CTRL", action = act.PasteFrom("PrimarySelection") },
 }
 
 local dimmer = { brightness = 0.05 }
 
 cfg.enable_scroll_bar = true
-cfg.min_scroll_bar_height = '2cell'
+cfg.min_scroll_bar_height = "2cell"
 cfg.background = {
-  {
-    source = {
-      File = w.home_dir .. "/.config/images/stones.jpg",
-    },
-    hsb = dimmer,
-    attachment = { Parallax = 0.1 },
-  },
-  {
-    source = {
-      File = w.home_dir .. "/.config/images/water.png",
-    },
-    hsb = dimmer,
-    attachment = { Parallax = 0.3 },
-  },
+	{
+		source = {
+			File = w.home_dir .. "/.config/images/stones.jpg",
+		},
+		hsb = dimmer,
+		attachment = { Parallax = 0.1 },
+	},
+	{
+		source = {
+			File = w.home_dir .. "/.config/images/water.png",
+		},
+		hsb = dimmer,
+		attachment = { Parallax = 0.3 },
+	},
 }
 
-print 'test'
+print("test")
 
 return cfg

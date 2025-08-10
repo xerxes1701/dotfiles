f16da9c76a16a3cba99b63a57979c14e24fe72c9
Michael Gawlik <michaelgawlik1983@gmail.com>
2025-08-11 01:16:33 +0200

    changed: nvim incremental selection bindings

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -33,10 +33,10 @@ return {
 			incremental_selection = {
 				enable = true,
 				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
+					init_selection = "<A-v>",
+					node_incremental = "<A-v>",
 					scope_incremental = false,
-					node_decremental = "<bs>",
+					node_decremental = "<A-V>",
 				},
 			},
 		})

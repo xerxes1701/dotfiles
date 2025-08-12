aeee216ec7cfa502a0b75afa5643b41dcd3c1df2
Michael Gawlik <michaelgawlik1983@gmail.com>
2025-08-12 22:09:42 +0200

    changed: nvim flash keybindings

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -25,8 +25,8 @@ return {
   -- stylua: ignore
   keys = {
     { "<k7>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "<F17><F17>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "<F17><F16>", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "<F17>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "<F18>", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },

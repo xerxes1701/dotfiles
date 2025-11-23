df377414250b03b5bd44a5ce20f217925b33f51b
Michael Gawlik <michaelgawlik1983@gmail.com>
2025-11-23 19:59:46 +0100

    added nvim treesitter objects

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,23 @@
 
 return {
 	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
 	build = ":TSUpdate",
+	init = function()
+		local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+
+		-- This repeats the last query with always previous direction and to the start of the range.
+		vim.keymap.set({ "n", "x", "o" }, "<home>", function()
+			ts_repeat_move.repeat_last_move({ forward = false, start = true })
+		end)
+
+		-- This repeats the last query with always next direction and to the end of the range.
+		vim.keymap.set({ "n", "x", "o" }, "<end>", function()
+			ts_repeat_move.repeat_last_move({ forward = true, start = false })
+		end)
+	end,
 	config = function()
 		require("nvim-treesitter.configs").setup({
 			auto_install = true,
@@ -37,6 +53,56 @@ return {
 					node_incremental = "<A-v>",
 					scope_incremental = false,
 					node_decremental = "<A-V>",
+				},
+			},
+			textobjects = {
+				-- `af`, `if`, `ac`, `ic`, etc.
+				select = {
+					enable = true,
+					lookahead = true, -- jump forward automatically
+
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+					},
+				},
+
+				-- ]m / [m etc.
+				move = {
+					enable = true,
+					set_jumps = true, -- put jumps in jumplist
+
+					goto_next_start = {
+						["]m"] = "@function.outer",
+						["]M"] = "@function.inner",
+						["]]"] = "@class.outer",
+						["]A"] = "@attribute.outer",
+						["]a"] = "@attribute.inner",
+						["]P"] = "@parameter.outer",
+						["]p"] = "@parameter.inner",
+					},
+					goto_previous_start = {
+						["[m"] = "@function.outer",
+						["[M"] = "@function.inner",
+						["[["] = "@class.outer",
+						["[A"] = "@attribute.outer",
+						["[a"] = "@attribute.inner",
+						["[P"] = "@parameter.outer",
+						["[p"] = "@parameter.inner",
+					},
+				},
+
+				-- swap parameters with <leader>a / <leader>A
+				swap = {
+					enable = true,
+					swap_next = {
+						["<leader>a"] = "@parameter.inner",
+					},
+					swap_previous = {
+						["<leader>A"] = "@parameter.inner",
+					},
 				},
 			},
 		})
